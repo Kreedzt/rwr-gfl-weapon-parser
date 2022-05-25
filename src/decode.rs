02fd5b69a0c541098681b53ca3902b3eba3f3306
Kreedzt <zhaozisong1@live.com>
2022-05-25 14:33:01 +0800

    📄  (add spdx license to file)

--- src/decode.rs
+++ src/decode.rs
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-only
 use encoding_rs::*;
 use std::fs::File;
 use std::io::Read;
--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-only
 use crate::Output;
 use quick_xml::events::BytesStart;
 use quick_xml::Reader;
